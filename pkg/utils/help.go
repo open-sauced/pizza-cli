fccc2076af6d239192926eacad8efb093c2a2b77
k1nho <kinhong99@gmail.com>
2023-08-07 14:49:55 -0400

    add extra newlines to help

--- pkg/utils/help.go
+++ pkg/utils/help.go
@@ -1,4 +1,6 @@
 package utils
 
-var helpTemplate = `{{with (or .Long .Short)}}{{. | trimTrailingWhitespaces}}
+var helpTemplate = `
+{{with (or .Long .Short)}}{{. | trimTrailingWhitespaces}}
+
 {{end}}{{if or .Runnable .HasSubCommands}}{{.UsageString}}{{end}}`
