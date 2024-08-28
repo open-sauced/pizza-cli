2a5f85de3bbe3a3d2812fcfe1ba4640ad7b55827
Nick Taylor <nick@nickyt.co>
2024-08-28 18:32:32 -0400

    test: added tests for LoadConfig

---
+++ pkg/config/config_test.go
@@ -0,0 +1,54 @@
+package config
+
+import (
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestLoadConfig(t *testing.T) {
+	// Create a temporary directory for test files
+	tmpDir, err := os.MkdirTemp("", "config_test")
+	if err != nil {
+		t.Fatalf("Failed to create temp directory: %v", err)
+	}
+	defer os.RemoveAll(tmpDir)
+
+	// Create an empty file for testing
+	configFilePath := filepath.Join(tmpDir, ".sauced.yaml")
+
+	if err := os.WriteFile(configFilePath, []byte{}, 0644); err != nil {
+		t.Fatalf("Failed to create empty file: %v", err)
+	}
+
+	// Path for a non-existent file
+	nonExistentPath := filepath.Join(tmpDir, "non_existent.yaml")
+
+	tests := []struct {
+		name          string
+		path          string
+		fallbackPath  string
+		expectedError bool
+	}{
+		{"Existing file", configFilePath, "", false},
+		{"Non-existent file", nonExistentPath, "", true},
+		{"Non-existent file with fallback", DefaultConfigPath, configFilePath, false},
+		{"Default path", DefaultConfigPath, "", true},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			config, err := LoadConfig(tt.path, tt.fallbackPath)
+
+			if tt.expectedError {
+				assert.Error(t, err)
+				assert.Nil(t, config)
+			} else {
+				assert.NoError(t, err)
+				assert.NotNil(t, config)
+			}
+		})
+	}
+}
