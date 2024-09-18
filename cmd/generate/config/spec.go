ee642736898addc236d01611bdf8e24dff635e57
Zeu Capua <coding@zeu.dev>
2024-09-18 09:03:57 -0700

    formatted

--- cmd/generate/config/spec.go
+++ cmd/generate/config/spec.go
@@ -89,7 +89,7 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 			}
 			return m, nil
 
-		case tea.KeyCtrlS: 
+		case tea.KeyCtrlS:
 			return m, runOutputGeneration(m.opts, m.attributionMap)
 
 		case tea.KeyEnter:
